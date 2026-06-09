1b61ae3454ae9f22599538e2009e0752451c1a1b
CaseDuncan <oluochdancun19@gmail.com>
2026-06-09 23:48:10 +0300

    fixing bugs

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -3,17 +3,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import CheckoutSteps from '../components/CheckoutSteps';
 import { createOrder } from '../actions/orderActions';
-function PlaceOrderScreen(props) {
+
+// Destructured history right from props for predictable dependency linting
+function PlaceOrderScreen({ history }) {
 
   const cart = useSelector(state => state.cart);
   const orderCreate = useSelector(state => state.orderCreate);
-  const { loading, success, error, order } = orderCreate;
+  // Removed unused 'loading' and 'error' variables
+  const { success, order } = orderCreate;
 
   const { cartItems, shipping, payment } = cart;
   if (!shipping.address) {
-    props.history.push("/shipping");
+    history.push("/shipping");
   } else if (!payment.paymentMethod) {
-    props.history.push("/payment");
+    history.push("/payment");
   }
   const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
   const shippingPrice = itemsPrice > 100 ? 0 : 10;
@@ -29,24 +32,22 @@ function PlaceOrderScreen(props) {
       taxPrice, totalPrice
     }));
   }
-  useEffect(() => {
-    if (success) {
-      props.history.push("/order/" + order._id);
-    }
 
-  }, [success]);
+  useEffect(() => {
+    if (success && order) {
+      history.push("/order/" + order._id);
+    }
+  }, [success, order, history]); // Fully tracks required reactive dependencies
 
   return <div>
     <CheckoutSteps step1 step2 step3 step4 ></CheckoutSteps>
     <div className="placeorder">
       <div className="placeorder-info">
         <div>
-          <h3>
-            Shipping
-          </h3>
+          <h3>Shipping</h3>
           <div>
             {cart.shipping.address}, {cart.shipping.city},
-          {cart.shipping.postalCode}, {cart.shipping.country},
+            {cart.shipping.postalCode}, {cart.shipping.country},
           </div>
         </div>
         <div>
@@ -58,21 +59,15 @@ function PlaceOrderScreen(props) {
         <div>
           <ul className="cart-list-container">
             <li>
-              <h3>
-                Shopping Cart
-          </h3>
-              <div>
-                Price
-          </div>
+              <h3>Shopping Cart</h3>
+              <div>Price</div>
             </li>
             {
               cartItems.length === 0 ?
-                <div>
-                  Cart is empty
-          </div>
+                <div>Cart is empty</div>
                 :
                 cartItems.map(item =>
-                  <li>
+                  <li key={item.product}>
                     <div className="cart-image">
                       <img src={item.image} alt="product" />
                     </div>
@@ -81,7 +76,6 @@ function PlaceOrderScreen(props) {
                         <Link to={"/product/" + item.product}>
                           {item.name}
                         </Link>
-
                       </div>
                       <div>
                         Qty: {item.qty}
@@ -95,9 +89,8 @@ function PlaceOrderScreen(props) {
             }
           </ul>
         </div>
-
-      
       </div>
+
       <div className="placeorder-action">
         <ul>
           <li>
@@ -123,14 +116,9 @@ function PlaceOrderScreen(props) {
             <div>${totalPrice}</div>
           </li>
         </ul>
-
-
-
       </div>
-
     </div>
   </div>
-
 }
 
 export default PlaceOrderScreen;
